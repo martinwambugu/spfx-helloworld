c22a76742e3086ae9c665aa58aaf268e3b52c6a3
Martin Wambugu <martin.wambugu@gmail.com>
2021-11-14 15:26:08 +0300

    first commit

---
+++ src/webparts/spFxHelloWorl/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpFxHelloWorlWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpFxHelloWorlWebPartStrings' {
+  const strings: ISpFxHelloWorlWebPartStrings;
+  export = strings;
+}
